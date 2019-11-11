0a60f5e2b50696256b5b5eed5e9638b6d1dc2569
sshubhi <55684944+sshubhi@users.noreply.github.com>
2019-11-10 23:39:06 -0800

    Add files via upload

--- predictor.c
+++ predictor.c
@@ -39,10 +39,22 @@ uint8_t *choice_predictor;
 uint32_t *localhist_table;
 uint32_t ghr;
 
+//hgp -> hybrid gshare perceptron 
+uint8_t *hgp_gshare_bht;
+uint8_t *hgp_choice_predictor;
+int8_t *hgp_perceptron_table;
+uint32_t hgp_ghr_size = 14;
+uint32_t hgp_perceptron_table_size = 8;
+uint32_t hgp_num_weight;
+int32_t theta;
+
+
 uint8_t bht_mask = 3;
 uint32_t localhist_mask;
 uint32_t pc_index_mask;
 uint32_t globalhist_mask;
+uint32_t hgp_ghr_mask;
+uint32_t hgp_perceptron_table_mask;
 
 
 
@@ -52,13 +64,7 @@ uint32_t globalhist_mask;
 //------------------------------------//
 
 uint8_t get_prediction_2b_cntr(uint8_t *cntr, uint32_t index)
-{	uint8_t mask = 2;
-	/*if (cntr == gshare_bht) {
-		printf("\ncntr is %d and index is %d", cntr,index);
-		printf("\n2bht is %d", *(cntr+index)); 
-		printf("\nprediction is %d",(*(cntr+index) & mask)>>1);
-	} */
-	printf("\nget_prediction. cntr is %d", cntr);	
+{	uint8_t mask = 2;	
 	return ((*(cntr+index) & mask)>>1);
 }
 
@@ -80,6 +86,72 @@ void update_local_pred_pattern(uint32_t *cntr, uint32_t index, uint8_t outcome)
 	*(cntr + index) = ((*(cntr+index) << 1) | outcome) & localhist_mask;
 }
 
+uint8_t perceptron_predict (uint32_t pc)
+{	uint8_t prediction;
+	int perceptron_output;
+	uint32_t pc_indexed = pc & hgp_perceptron_table_mask;
+	
+	for (int i=0; i<hgp_num_weight; i++) {
+		if (i == 0)
+			perceptron_output = *(hgp_perceptron_table + pc_indexed);
+		else {
+			uint32_t temp;
+			temp = ghr & i;
+			temp = temp >> (i-1);
+			if (temp)
+				perceptron_output += *(hgp_perceptron_table + pc_indexed + i);
+			else
+				perceptron_output -= *(hgp_perceptron_table + pc_indexed + i);
+		}
+	}
+	if (perceptron_output > 0)
+		prediction = 1;
+	else 
+		prediction = 0;
+	return prediction;	
+}
+
+int perceptron_calc_output (uint32_t pc)
+{
+	int perceptron_output;
+	uint32_t pc_indexed = pc & hgp_perceptron_table_mask;
+	
+	for (int i=0; i<hgp_num_weight; i++) {
+		if (i == 0)
+			perceptron_output = *(hgp_perceptron_table + pc_indexed);
+		else {
+			uint32_t temp;
+			temp = ghr & i;
+			temp = temp >> (i-1);
+			if (temp)
+				perceptron_output += *(hgp_perceptron_table + pc_indexed + i);
+			else
+				perceptron_output -= *(hgp_perceptron_table + pc_indexed + i);
+		}
+	}
+	return perceptron_output;	
+}
+void perceptron_train (uint32_t pc, uint8_t outcome)
+{
+	uint32_t pc_indexed = pc & hgp_perceptron_table_mask;	
+	
+	if ((perceptron_predict(pc) != outcome) || (abs(perceptron_calc_output(pc)) < theta)) {
+		for (int i=0; i<hgp_num_weight; i++) {
+			if (i == 0)
+				*(hgp_perceptron_table + pc_indexed) = *(hgp_perceptron_table + pc_indexed) + 1;
+			else {
+				uint32_t temp;
+				temp = ghr & i;
+				temp = temp >> (i-1);
+				if (temp)
+					*(hgp_perceptron_table + pc_indexed + i) = *(hgp_perceptron_table + pc_indexed + i) + 1;
+				else
+					*(hgp_perceptron_table + pc_indexed + i) = *(hgp_perceptron_table + pc_indexed + i) - 1;
+			}
+		}		
+	}
+}
+
 // Initialize the predictor
 //
 void
@@ -90,13 +162,39 @@ init_predictor()
   pc_index_mask = (1<<pcIndexBits) - 1;
   globalhist_mask = (1<<ghistoryBits) - 1;
   ghr = 0;
-  gshare_bht = (uint8_t *)calloc(1, (globalhist_mask + 1)*sizeof(uint8_t));
-  localpred_bht = (uint8_t *)calloc(1, (localhist_mask+1)*sizeof(localpred_bht));
-  globalpred_bht = (uint8_t *)calloc(1, (globalhist_mask+1)*sizeof(globalpred_bht));
-  choice_predictor = (uint8_t *)calloc(0, (globalhist_mask+1)*sizeof(choice_predictor));
-  localhist_table = (uint32_t *)calloc(2, (pc_index_mask+1)*sizeof(localhist_table)); 
-  printf("\ninit_prediction");
-  printf("\n gshare_bht is: %d, localpred_bht is: %d, globalpred_bht is: %d, choice_predictor is:%d",gshare_bht,localpred_bht,globalpred_bht,choice_predictor);
+  gshare_bht = (uint8_t *)calloc((globalhist_mask + 1), sizeof(uint8_t));
+  localpred_bht = (uint8_t *)calloc((localhist_mask+1),sizeof(uint8_t));
+  globalpred_bht = (uint8_t *)calloc((globalhist_mask+1),sizeof(uint8_t));
+  choice_predictor = (uint8_t *)calloc((globalhist_mask+1),sizeof(uint8_t));
+  localhist_table = (uint32_t *)calloc((pc_index_mask+1),sizeof(uint32_t)); 
+  for (int i=0; i<globalhist_mask+1; i++){
+	  *(gshare_bht+i) = 1;
+	  *(globalpred_bht + i) = 1;
+	  *(choice_predictor + i) = 2;
+  }
+  for (int i=0; i<localhist_mask+1; i++){
+	  *(localpred_bht + i) = 1;
+  }
+  for (int i=0; i<pc_index_mask+1; i++){
+	  *(localhist_table + i) = 0;	
+  }
+  //Custom Predictor
+  hgp_num_weight = hgp_ghr_size + 1;
+  theta = (1.93*hgp_ghr_size) + 14;
+  hgp_ghr_mask = hgp_ghr_size - 1;
+  hgp_perceptron_table_mask = hgp_perceptron_table_size - 1;
+  
+  hgp_gshare_bht = (uint8_t *)calloc((hgp_ghr_size), sizeof(uint8_t));
+  hgp_choice_predictor = (uint8_t *)calloc((hgp_ghr_size), sizeof(uint8_t));
+  hgp_perceptron_table = (uint8_t *)calloc((hgp_perceptron_table_size), sizeof(uint32_t));
+
+  for (int i=0; i<hgp_ghr_size;i++) {
+	  *(hgp_gshare_bht + i) = 1;
+	  *(hgp_choice_predictor + i) = 2;
+  }
+  for (int i=0; i<hgp_perceptron_table_size;i++) {
+	  *(hgp_perceptron_table + i) = 0;
+  }
 }
 
 // Make a prediction for conditional branch instruction at PC 'pc'
@@ -108,17 +206,16 @@ make_prediction(uint32_t pc)
 {	uint8_t	prediction;
 
     //Gshare
-	uint32_t pc_xor_ghr = (pc ^ ghr) & globalhist_mask;
-	//printf ("\n pc is %d, ghr is %d, xor is %d, mask is %d", pc,ghr,pc_xor_ghr,globalhist_mask);
-	//printf("\n pc_index_mask is %d, pc is %d, pc_xor_ghr is %d", pc_index_mask, pc, pc_xor_ghr);
-	//printf ("\n Other masks: %d, %d", globalhist_mask, localhist_mask);
-	printf("\nmake_prediction");
+	uint32_t pc_xor_ghr = (pc & globalhist_mask) ^ (ghr & globalhist_mask);
+
 	//Tournament
 	uint32_t pc_index = pc & pc_index_mask;
 	uint32_t localpred_index = get_local_pred_pattern(localhist_table,pc_index);
     uint32_t globalpred_index = ghr & globalhist_mask;
 	
 	//Custom
+	uint32_t hgp_gshare_xor_index = (pc ^ ghr) & hgp_ghr_mask;
+	uint32_t hgp_choice_pred_index = ghr & hgp_ghr_mask;
 
   // Make a prediction based on the bpType
   switch (bpType) {
@@ -131,7 +228,10 @@ make_prediction(uint32_t pc)
 		prediction = (get_prediction_2b_cntr(choice_predictor,globalpred_index)) ? get_prediction_2b_cntr(globalpred_bht,globalpred_index) : get_prediction_2b_cntr(localpred_bht,localpred_index);
 		return prediction;
 	}
-    case CUSTOM:
+    case CUSTOM: {
+		prediction = (get_prediction_2b_cntr(hgp_choice_predictor,hgp_choice_pred_index)) ? perceptron_predict(pc) : get_prediction_2b_cntr(hgp_gshare_bht,hgp_gshare_xor_index);
+		return prediction;
+	}
     default:
       break;
   }
@@ -149,24 +249,38 @@ void
 train_predictor(uint32_t pc, uint8_t outcome)
 {	
 	//Gshare
-	uint32_t pc_xor_ghr = (pc ^ ghr) & globalhist_mask;
+	uint32_t pc_xor_ghr = (pc & globalhist_mask) ^ (ghr & globalhist_mask);
 	
 	//Tournament
 	uint32_t pc_index = pc & pc_index_mask;
 	uint32_t localpred_index = get_local_pred_pattern(localhist_table,pc_index);
     uint32_t globalpred_index = ghr & globalhist_mask;
-	printf("\n update_predictor index is %d",pc_xor_ghr);
-	update_2b_cntr(gshare_bht,pc_xor_ghr,outcome);
-	update_2b_cntr(localpred_bht,localpred_index,outcome);
-	update_2b_cntr(globalpred_bht,globalpred_index,outcome);
 	
-	//Update choice_predictor 
+	//Custom
+	uint32_t hgp_gshare_xor_index = (pc ^ ghr) & hgp_ghr_mask;
+	uint32_t hgp_choice_pred_index = ghr & hgp_ghr_mask;	
+	
+	//Update choice_predictor for tournament predictor
 	if ((get_prediction_2b_cntr(localpred_bht,localpred_index) == outcome) && (get_prediction_2b_cntr(globalpred_bht,globalpred_index) != outcome)) {
 		update_2b_cntr(choice_predictor,globalpred_index,(0));
 	}
 	else if ((get_prediction_2b_cntr(localpred_bht,localpred_index) != outcome) && (get_prediction_2b_cntr(globalpred_bht,globalpred_index) == outcome)) {
 		update_2b_cntr(choice_predictor,globalpred_index,(1));
-	}		
+	}	
+	
+	//Update choice_predictor for custom predictor (hybrid gshare perceptron predictor)
+	if ((get_prediction_2b_cntr(hgp_gshare_bht,hgp_gshare_xor_index) == outcome) && (perceptron_predict(pc) != outcome)) {
+		update_2b_cntr(hgp_choice_predictor,hgp_choice_pred_index,(0));
+	}
+	else if ((get_prediction_2b_cntr(hgp_gshare_bht,hgp_gshare_xor_index) != outcome) && (perceptron_predict(pc) == outcome)) {
+		update_2b_cntr(hgp_choice_predictor,hgp_choice_pred_index,(1));
+	}	
+
+	update_2b_cntr(gshare_bht,pc_xor_ghr,outcome);
+	update_2b_cntr(localpred_bht,localpred_index,outcome);
+	update_2b_cntr(globalpred_bht,globalpred_index,outcome);	
+	update_2b_cntr(hgp_gshare_bht,hgp_gshare_xor_index,outcome);
+	perceptron_train(pc,outcome);	
 	ghr = (ghr << 1) | outcome;
 	update_local_pred_pattern(localhist_table,pc_index,outcome);
 	
